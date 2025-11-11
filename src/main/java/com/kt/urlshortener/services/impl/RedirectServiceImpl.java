4b11d8798841cf252c3b2fe0b3947759a5efaded
KonBanDiawKan <khamteerapat@hotmail.com>
2025-11-11 10:13:02 +0700

    add cache when fetch originalUrl

--- src/main/java/com/kt/urlshortener/services/impl/RedirectServiceImpl.java
+++ src/main/java/com/kt/urlshortener/services/impl/RedirectServiceImpl.java
@@ -3,6 +3,7 @@ package com.kt.urlshortener.services.impl;
 import com.kt.urlshortener.repositorys.LinksMappingRepository;
 import com.kt.urlshortener.services.RedirectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class RedirectServiceImpl implements RedirectService {
     private final LinksMappingRepository linksMappingRepository;
 
     @Override
+    @Cacheable(value = "redirect_cache",key = "#shortCode")
     public String getOriginalUrlForRedirect(String shortCode) {
         return linksMappingRepository.getOriginalUrlByShortCode(shortCode);
     }
