41ae18b13f6c3fb9b65b3927688e7bcbbd37272d
khamteerapat <77033711+khamteerapat@users.noreply.github.com>
2025-11-12 16:52:00 +0700

    add validate url

--- src/main/java/com/kt/urlshortener/controllers/UrlShortenerController.java
+++ src/main/java/com/kt/urlshortener/controllers/UrlShortenerController.java
@@ -5,6 +5,8 @@ import com.kt.urlshortener.payloads.UrlShortenerResponsePayload;
 import com.kt.urlshortener.services.UrlShortenerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +21,10 @@ public class UrlShortenerController {
     private final UrlShortenerService urlShortenerService;
 
     @PostMapping
-    public ResponseEntity<UrlShortenerResponsePayload> shortenUrl(@RequestBody @Valid UrlShortenerRequestPayload requestPayload){
+    public ResponseEntity<UrlShortenerResponsePayload> shortenUrl(@RequestBody @Valid UrlShortenerRequestPayload requestPayload) throws BadRequestException {
+        if(!UrlValidator.getInstance().isValid(requestPayload.getOriginalUrl())){
+            throw new BadRequestException("url format invalid.");
+        }
         String shortUrl = urlShortenerService.generateShortUrl(requestPayload.getOriginalUrl());
         return ResponseEntity.ok(new UrlShortenerResponsePayload(shortUrl));
     }
